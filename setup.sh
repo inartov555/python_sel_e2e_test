1adae5055686e881fbd860620f509dc46051ee08
Iurii Nartov <yurahttp@gmail.com>
2025-10-10 01:08:15 +0300

    PyLint: Your code has been rated at 10/10

--- setup.sh
+++ setup.sh
@@ -5,6 +5,7 @@
 # Exported variables: HOST_ARTIFACTS, ROOT_VENV, TEST_VENV, COPIED_PROJECT_PATH
 
 ARTIFACTS_ROOT_FOLDER="TEST1"
+
 # DEFAULT_REPO_PATH="$HOME/python-e2e-test"
 
 # Repo path defaults to $DEFAULT_REPO_PATH
