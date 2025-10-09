475081ad89061ee7110d557179dc0601d86cf429
Iurii Nartov <yurahttp@gmail.com>
2025-10-09 23:14:41 +0300

    PyLint: Your code has been rated at 9.42/10

--- tests/test_auth_flow.py
+++ tests/test_auth_flow.py
@@ -3,7 +3,6 @@ Tests for an authorized user
 """
 
 from __future__ import annotations
-import os
 
 import pytest
 
@@ -58,17 +57,3 @@ class TestFeedAuth:
         first_post.scroll_to_element_liked_by()
         first_post.expect_comment_button_visible()
         first_post.save()
-
-'''
-def test_login_and_feed_visible(login_page, home_feed_page, creds):
-    if not creds["username"] or not creds["password"]:
-        pytest.skip("Provide --username/--password or IG_USERNAME/IG_PASSWORD to run auth test")
-
-    login_page.open()
-    login_page.allow_all_cookies_if_shown()
-    login_page.expect_loaded()
-    login_page.form().login(creds["username"], creds["password"])
-
-    home_feed_page.open()
-    home_feed_page.expect_loaded()
-'''
--- tests/test_public_pages.py
+++ tests/test_public_pages.py
@@ -54,34 +54,3 @@ class TestPublicPages:
         self.login_page.expect_loaded()
         self.login_page.login("incorrect_user@example.com", "wrong_password")
         self.login_page.expect_error_login()
-
-
-'''
-def test_landing_page_loads(landing_page):
-    landing_page.open()
-    landing_page.accept_cookies_if_shown()
-    landing_page.expect_loaded()
-
-def test_login_page_elements(login_page):
-    login_page.open()
-    login_page.allow_all_cookies_if_shown()
-    login_page.expect_loaded()
-
-def test_signup_page_elements(signup_page):
-    signup_page.open()
-    signup_page.accept_cookies_if_shown()
-    signup_page.expect_loaded()
-
-def test_login_negative_incorrect_creds(login_page):
-    login_page.open()
-    login_page.allow_all_cookies_if_shown()
-    login_page.expect_loaded()
-    form = login_page.form()
-    form.login("invalid_user_123456", "wrong_password_123")
-    errs = form.ui.finds(form.ERROR_AREA)
-    assert errs is not None
-
-def test_cookie_banner_optional(landing_page):
-    landing_page.open()
-    _ = landing_page.accept_cookies_if_shown()
-'''
